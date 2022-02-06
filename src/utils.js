91d6d05df1d87445a146e800210d47df89066467
afkhalid <ahmed@qbdvision.com>
2022-02-06 17:40:56 +0200

    Cleaning files

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,5 @@
-const DOMPurify = require("dompurify");
-
-export function getParameterByName(name, separator = "?", props) {
+export function getParameterByName(name, props, separator = "?") {
   const searchString = props.location.search;
   const match = new RegExp(`[${separator}&]` + name + "=([^&]*)").exec(searchString);
-  const param = match && decodeURIComponent(match[1].replace(/\+/g, " "));
-  return param ? (DOMPurify.sanitize(param) || "").toString() : param;
+  return match && decodeURIComponent(match[1].replace(/\+/g, " "));
 }
