8094eaabe603e2a1f962aaadb8a8d69fdfddf752
afkhalid <ahmed@qbdvision.com>
2022-02-06 16:09:59 +0200

    First deployment to FireBase

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HomePage from "./home_page";
+import logo from "./images/logo.png";
+import ScanPage from "./scan";
+
+const LOGO_SIZE = 192;
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div className="content">
+      <div className="centered">
+        <a href="/">
+          <img src={logo}
+               alt="Bike Rental"
+               width={LOGO_SIZE}
+               height={LOGO_SIZE} />
+        </a>
+      </div>
+      <Route path="/" exact component={() => <HomePage />} />
+      <Route path="/scan.html" exact component={(props) => <ScanPage {...props} />} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
