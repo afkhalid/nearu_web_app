6b59fa05d6561269d57aac1b4edeeb8482f30076
afkhalid <ahmed@qbdvision.com>
2022-04-27 00:14:48 +0200

    Made with love

--- src/confirmation.jsx
+++ src/confirmation.jsx
@@ -1,5 +1,7 @@
 import { Component } from "react";
 import confirmation from "./images/confirmation.svg";
+import ios from "./images/ios.svg";
+import android from "./images/android.svg";
 
 export default class ConfirmationPage extends Component {
   render() {
@@ -11,6 +13,29 @@ export default class ConfirmationPage extends Component {
           />
           <div className="message-sent-header">THANK YOU!</div>
           <div className="message-sub-header">Your message has been sent successfully!</div>
+          <div className="download-our-app-header">DOWNLOAD OUR APP FOR <span>FREE</span>!</div>
+          <div className="store-buttons">
+            <div>
+              <a target="_blank"
+                 href="https://apps.apple.com/eg/app/nearu-protect-what-matters/id1616418670"
+              >
+                <img src={ios}
+                     width={180}
+                     alt="iOS Store Button"
+                />
+              </a>
+            </div>
+            <div>
+              <a target="_blank"
+                 href="https://play.google.com/store/apps/details?id=nearu.nearu"
+              >
+                <img src={android}
+                     width={180}
+                     alt="Play Store Button"
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import HomePage from "./home_page";
@@ -10,11 +10,14 @@ import ConfirmationPage from "./confirmation";
 
 ReactDOM.render(
   <BrowserRouter>
-    <div className="content">
-      <Route path="/" exact component={() => <HomePage />} />
-      <Route path="/scan.html" exact component={(props) => <ScanPage {...props} />} />
-      <Route path="/confirmation.html" exact component={(props) => <ConfirmationPage {...props} />} />
-    </div>
+    <Fragment>
+      <div className="content">
+        <Route path="/" exact component={() => <HomePage />} />
+        <Route path="/scan.html" exact component={(props) => <ScanPage {...props} />} />
+        <Route path="/confirmation.html" exact component={(props) => <ConfirmationPage {...props} />} />
+      </div>
+      <div className="made-with-love">Made with ❤ by NearU!</div>
+    </Fragment>
   </BrowserRouter>,
   document.getElementById('root')
 );
