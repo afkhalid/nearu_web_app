10e1835a8bbaeaf5ac76f0e0ae2bf6c0b30f73dc
afkhalid <ahmed@qbdvision.com>
2022-05-06 19:35:59 +0200

    Making CI a bit better

--- src/confirmation.jsx
+++ src/confirmation.jsx
@@ -6,18 +6,16 @@ import MadeWithLove from "./widgets/made_with_love";
 export default class ConfirmationPage extends Component {
   render() {
     return (
-      <div>
-        <div className="content">
-          <div className="send-message-logo">
-            <img src={confirmation}
-                 alt="Message Sent!"
-            />
-            <div className="message-sent-header">THANK YOU!</div>
-            <div className="message-sub-header">Your message has been sent successfully!</div>
-            <DownloadApp showTitle={true} />
-          </div>
-          <MadeWithLove />
+      <div className="content">
+        <div className="send-message-logo">
+          <img src={confirmation}
+               alt="Message Sent!"
+          />
+          <div className="message-sent-header">THANK YOU!</div>
+          <div className="message-sub-header">Your message has been sent successfully!</div>
+          <DownloadApp showTitle={true} />
         </div>
+        <MadeWithLove />
       </div>
     );
   }
--- src/tag_not_found.jsx
+++ src/tag_not_found.jsx
@@ -6,18 +6,16 @@ import MadeWithLove from "./widgets/made_with_love";
 export default class TagNotFoundPage extends Component {
   render() {
     return (
-      <div>
-        <div className="content">
-          <div className="send-message-logo">
-            <img src={notFound}
-                 alt="Tag not found!"
-            />
-            <div className="tag-not-found-header">THIS TAG IS UNCLAIMED!</div>
-            <div className="message-sub-header">Download NearU to unlock its powers</div>
-            <DownloadApp showTitle={false} />
-          </div>
-          <MadeWithLove />
+      <div className="content">
+        <div className="send-message-logo">
+          <img src={notFound}
+               alt="Tag not found!"
+          />
+          <div className="tag-not-found-header">THIS TAG IS UNCLAIMED!</div>
+          <div className="message-sub-header">Download NearU to unlock its powers</div>
+          <DownloadApp showTitle={false} />
         </div>
+        <MadeWithLove />
       </div>
     );
   }
