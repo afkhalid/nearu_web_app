190e5c6ed304570f3b1e4992a33712e098782d34
Tarek Radwan <tarekradwan@qbdvision.com>
2022-05-13 09:22:33 +0200

    Many many fixes

--- src/tag_not_found.jsx
+++ src/tag_not_found.jsx
@@ -1,22 +1,21 @@
-import { Component } from "react";
 import notFound from "./images/not_found.svg";
 import DownloadApp from "./widgets/download_app";
 import MadeWithLove from "./widgets/made_with_love";
 
-export default class TagNotFoundPage extends Component {
-  render() {
-    return (
-      <div className="content">
-        <div className="send-message-logo">
-          <img src={notFound}
-               alt="Tag not found!"
-          />
-          <div className="tag-not-found-header">THIS TAG IS UNCLAIMED!</div>
-          <div className="message-sub-header">Download NearU to unlock its powers</div>
-          <DownloadApp showTitle={false} />
+function TagNotFoundPage() {
+  return (
+    <div className="content">
+      <div className="send-message-logo">
+        <img src={notFound} alt="Tag not found!" />
+        <div className="tag-not-found-header">THIS TAG IS UNCLAIMED</div>
+        <div className="tertiary-text">
+          Download NearU to unlock its powers!
         </div>
-        <MadeWithLove />
       </div>
-    );
-  }
+      <DownloadApp showTitle={false} />
+      <MadeWithLove />
+    </div>
+  );
 }
+
+export default TagNotFoundPage;
