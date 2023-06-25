c9265cc9d30da38f3676fb7631a87669f1f88c92
Tarek Radwan <tarekradwan@qbdvision.com>
2023-06-25 19:51:14 +0300

    Feature: validate phone number
    Refactor: change into functional component

--- src/scan.jsx
+++ src/scan.jsx
@@ -1,9 +1,8 @@
-import { Component } from "react";
-import { getParameterByName, isMobile } from "./utils";
+import { useState, useEffect } from "react";
+import { getParameterByName, isMobile, isValidPhoneNumber } from "./utils";
 import logo from "./images/logo.svg";
 import { Alert, Button, Form, Spinner } from "react-bootstrap";
-import FIRE_STORE_CONFIG from "./configs/firestore.json";
-import { initializeApp } from "firebase/app";
+import fireBaseApp from "./configs/firebaseInit.js";
 import {
   getFunctions,
   connectFunctionsEmulator,
@@ -11,159 +10,176 @@ import {
 } from "firebase/functions";
 import MadeWithLove from "./widgets/made_with_love";
 
-export default class ScanPage extends Component {
-  constructor(props) {
-    super(props);
+export default function ScanPage(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tag, setTag] = useState(null);
+  const [showPhoneNumber, setShowPhoneNumber] = useState(false);
+  const [payload, setPayload] = useState({
+    message: "",
+    phone: "",
+  });
 
-    const code = getParameterByName("code", this.props);
-    window.history.replaceState({}, "", "/scan.html");
+  const tagUuid = getParameterByName("code", props);
+  const functions = getFunctions(fireBaseApp);
+  const getUserInformation = httpsCallable(functions, "getUserInformation");
+  // connectFunctionsEmulator(functions, "localhost", 5001);"
 
-    this.fireBaseApplication = initializeApp(FIRE_STORE_CONFIG);
-    this.functions = getFunctions(this.fireBaseApplication);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const tag = await getUserInformation({ code: tagUuid });
 
-    // connectFunctionsEmulator(this.functions, "localhost", 5001);
-
-    this.state = {
-      isLoading: true,
-      showPhoneNumber: false,
-      code,
+        if (!tag || (tag && !tag.data)) {
+          setIsLoading(false);
+          window.open("/tagNotFound.html", "_self");
+        } else {
+          setIsLoading(false);
+          setTag(tag.data);
+        }
+      } catch (error) {
+        // Handle error if necessary
+      }
     };
-  }
 
-  async componentDidMount() {
-    const getUserInformation = httpsCallable(
-      this.functions,
-      "getUserInformation"
-    );
-    const tag = await getUserInformation({ code: this.state.code });
+    fetchData();
+    // window.history.replaceState({}, "", "/scan.html");
+  }, [getUserInformation, tagUuid]);
 
-    if (!tag || (tag && !tag.data)) {
-      this.setState({ isLoading: false });
-      window.open("/tagNotFound.html", "_self");
-    } else {
-      this.setState({ isLoading: false, tag: tag.data });
-    }
-  }
-
-  handleNumberOperation() {
+  const handleNumberOperation = () => {
     if (isMobile()) {
-      window.open(this.state.tag.phoneNumber);
+      window.open(tag.phoneNumber);
     } else {
-      this.setState({ showPhoneNumber: true });
+      setShowPhoneNumber(true);
     }
-  }
+  };
 
-  handleUpdateText(fieldName, e) {
+  const handleUpdateText = (fieldName, e) => {
     const text = e.target.value;
-    this.setState({ [fieldName]: text });
-  }
+    setPayload((prevState) => ({
+      ...prevState,
+      [fieldName]: text,
+    }));
+  };
 
-  async handleSendMessage(e) {
+  const handleSendMessage = async (e) => {
     e.preventDefault();
-    this.setState({ isLoading: true }, async () => {
-      const { code, message, phone } = this.state;
-      const sendMessageFunction = httpsCallable(this.functions, "sendMessage");
-      const result = await sendMessageFunction({ code, message, phone });
+    setIsLoading({ isLoading: true });
+
+    try {
+      const { message, phone } = payload;
+      const sendMessageFunction = httpsCallable(functions, "sendMessage");
+      const result = await sendMessageFunction({
+        code: tagUuid,
+        message,
+        phone,
+      });
+
       if (result?.data) {
-        this.setState({ isLoading: false });
+        setIsLoading(false);
         window.open("/confirmation.html", "_self");
       }
-    });
-  }
+    } catch (error) {
+      // Handle error if necessary
+      setIsLoading(false);
+    }
+  };
 
-  render() {
-    const { isLoading, tag, showPhoneNumber } = this.state;
-    return (
-      <div className="scan-page">
-        <div className="content">
-          <div className="send-message-logo">
-            <img src={logo} width={100} height={130} alt="NearU Logo" />
-          </div>
-          <div className="message-container">
-            <div className="inner-message-container">
-              <div className="send-message-header">CONTACT OWNER</div>
-              {tag?.additionalInformation ? (
-                <Alert className="mt-2" variant="info">
-                  <Alert.Heading as="h5">Note from the Owner</Alert.Heading>
-                  {tag.additionalInformation}
-                </Alert>
-              ) : (
-                ""
-              )}
-              <Form onSubmit={this.handleSendMessage.bind(this)}>
-                <Form.Group className="mb-3">
-                  <Form.Label>Enter your mobile number</Form.Label>
-                  <Form.Control
-                    type="tel"
-                    disabled={!tag}
-                    onChange={this.handleUpdateText.bind(this, "phone")}
-                    placeholder="+20-xxx-xxx-xxxx"
-                  />
-                  <div className="send-message-sub-header">
-                    So the owner can call you back
-                  </div>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="form-message">
-                  <Form.Label>Message</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    placeholder="Write your message here.."
-                    disabled={!tag}
-                    onChange={this.handleUpdateText.bind(this, "message")}
-                    rows={4}
-                    required
-                  />
-                </Form.Group>
-                <Form.Group className="mt-5">
-                  <Button variant="primary" disabled={!tag} type="submit">
-                    Send Message
-                  </Button>
-                  {tag && tag.showPhoneNumberWhenScanned ? (
-                    isMobile() ? (
-                      <a href={`tel:${tag.phoneNumber}`}>
-                        <Button
-                          className="show-number-button"
-                          variant="secondary"
-                          disabled={!tag}
-                        >
-                          Call Owner
-                        </Button>
-                      </a>
-                    ) : (
+  return (
+    <div className="scan-page">
+      <div className="content">
+        <div className="send-message-logo">
+          <img src={logo} width={100} height={130} alt="NearU Logo" />
+        </div>
+        <div className="message-container">
+          <div className="inner-message-container">
+            <div className="send-message-header">CONTACT OWNER</div>
+            {tag?.additionalInformation ? (
+              <Alert className="mt-2" variant="info">
+                <Alert.Heading as="h5">Note from the Owner</Alert.Heading>
+                {tag.additionalInformation}
+              </Alert>
+            ) : (
+              ""
+            )}
+            <Form onSubmit={handleSendMessage}>
+              <Form.Group className="mb-3">
+                <Form.Label>Enter your phone number</Form.Label>
+                <Form.Control
+                  type="tel"
+                  disabled={!tag}
+                  onChange={(e) => handleUpdateText("phone", e)}
+                  placeholder="+20-xxx-xxx-xxxx"
+                  isInvalid={
+                    payload.phone && !isValidPhoneNumber(payload.phone)
+                  }
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter a valid phone number.
+                </Form.Control.Feedback>
+                <Form.Text className="text-muted">
+                  So the owner can call you back
+                </Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="form-message">
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  placeholder="Write your message here.."
+                  disabled={!tag}
+                  onChange={(e) => handleUpdateText("message", e)}
+                  rows={4}
+                  required
+                />
+              </Form.Group>
+              <Form.Group className="mt-5">
+                <Button variant="primary" disabled={!tag} type="submit">
+                  Send Message
+                </Button>
+                {tag && tag.showPhoneNumberWhenScanned ? (
+                  isMobile() ? (
+                    <a href={`tel:${tag.phoneNumber}`}>
                       <Button
                         className="show-number-button"
                         variant="secondary"
                         disabled={!tag}
-                        onClick={this.handleNumberOperation.bind(this)}
                       >
-                        {showPhoneNumber ? tag.phoneNumber : "Show Number"}
+                        Call Owner
                       </Button>
-                    )
+                    </a>
                   ) : (
-                    ""
-                  )}
-                  {isLoading && !tag ? (
-                    <Button className="show-number-button" variant="secondary">
-                      <Spinner
-                        as="span"
-                        animation="grow"
-                        className="loading"
-                        role="status"
-                        size="sm"
-                        aria-hidden="true"
-                      />
-                      Loading
+                    <Button
+                      className="show-number-button"
+                      variant="secondary"
+                      disabled={!tag}
+                      onClick={handleNumberOperation}
+                    >
+                      {showPhoneNumber ? tag.phoneNumber : "Show Number"}
                     </Button>
-                  ) : (
-                    ""
-                  )}
-                </Form.Group>
-              </Form>
-            </div>
+                  )
+                ) : (
+                  ""
+                )}
+                {isLoading && !tag ? (
+                  <Button className="show-number-button" variant="secondary">
+                    <Spinner
+                      as="span"
+                      animation="grow"
+                      className="loading"
+                      role="status"
+                      size="sm"
+                      aria-hidden="true"
+                    />
+                    Loading
+                  </Button>
+                ) : (
+                  ""
+                )}
+              </Form.Group>
+            </Form>
           </div>
-          <MadeWithLove />
         </div>
+        <MadeWithLove />
       </div>
-    );
-  }
+    </div>
+  );
 }
